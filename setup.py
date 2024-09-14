67697920616cc6c2591ef1839ecc3af6391fc6ea
Yulia <ulechek.kul@gmail.com>
2024-09-14 12:22:44 -0400

    Added jupiter notebook file for experiments

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='vectordb',
+    name='chromatst',
     version='0.0.1',
     author='Yulia',
     author_email='ulechek.kul@gmail.com',
-    install_requires=["openai","langchain", "langchain-community", "langchain-openai", "python-dotenv","pypdf", "pinecone-client","tiktoken"],
+    install_requires=["openai","langchain", "langchain-community", "langchain-openai", 
+                      "langchain-chroma",
+                      "python-dotenv","pypdf", "chromadb","tiktoken"],
     packages=find_packages()
 
 )
